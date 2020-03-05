90af54b6ae303fe8863df26b2276e4623e3de817
Angeleen MCDONNELL Wilson <angeleenmc@gmail.com>
2020-03-05 03:37:40 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -39,26 +39,6 @@ class Student
       DB[:conn].execute(sql, self.name, self.grade)
     end
   end 
-  
-  def self.create_table
-    sql = <<-SQL
-      CREATE TABLE IF NOT EXISTS students (
-        id INTEGER PRIMARY KEY,
-        name TEXT,
-        grade TEXT
-        )
-      SQL
-        DB[:conn].execute(sql)
-    end
-  end 
-  
-  def self.drop_table
-    sql = <<-SQL
-      DROP TABLE students
-      SQL
-        DB[:conn].execute(sql)
-    end
-  end 
 
   def self.all_students_in_grade_9(grade)
     sql <<-SQL
